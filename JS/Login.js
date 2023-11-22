03b35a18c579e37d36bccf2c7c913b304e39d0a0
PatrickZablocki <Patrick Zablocki>
2023-11-22 12:28:50 +0100

    ich habe nun die function eingefügt für den Benutzername speichern

--- JS/Login.js
+++ JS/Login.js
@@ -1,13 +1,14 @@
-function animateSwitch() {
+//  Hier Rufe ich die Function ab in der wir im Projekt den Benutzername Speichern können 
+
+function toggleSwitch() {
     const switchElement = document.getElementById('switch');
-    const circle = document.getElementById('circle');
-    const username = prompt('Bitte Benutzernamen eingeben:', '');
-    
-    
-    if (username) {
-    console.log('Benutzername:', username);
-    
-    switchElement.style.backgroundColor = 'green';
-    circle.style.transform = 'translateX(50%)';
-    }
+    const circleElement = document.getElementById('circle');
+
+    // Hier Ändere ich die Position des Kreises
+    circleElement.style.transform = circleElement.style.transform === 'translateX(15px)' ? 'translateX(0)' : 'translateX(15px)';
+
+    // Hier Ändere ich die Hintergrundfarbe des Schalters
+    switchElement.style.backgroundColor = switchElement.style.backgroundColor === 'green' ? '#6c6a6a' : 'green';
+    switchElement.classList.toggle('active');
 }
+// 

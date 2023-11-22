9e98c24c7c48f36699834ba0078ced8723fa19c7
PatrickZablocki <Patrick Zablocki>
2023-11-22 18:15:32 +0100

    Die ersten Funktionen sind geschrieben damit wir uns anmelden können

--- JS/Login.js
+++ JS/Login.js
@@ -11,4 +11,39 @@ function toggleSwitch() {
     switchElement.style.backgroundColor = switchElement.style.backgroundColor === 'green' ? '#6c6a6a' : 'green';
     switchElement.classList.toggle('active');
 }
-// 
+// Ab hier muss ich die Anmeldungen Speicher etc.
+
+function registerUser(username, password) {
+    
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        console.log('Benutzer existiert bereits.');
+        return false;
+    }
+
+    const newUser = { username, password };
+    users.push(newUser);
+
+    
+    localStorage.setItem('users', JSON.stringify(users));
+
+    console.log('Benutzer erfolgreich registriert.');
+    return true;
+}
+function loginUser(username, password) {
+    
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+
+    const authenticatedUser = users.find(user => user.username === username && user.password === password);
+    if (authenticatedUser) {
+        console.log('Anmeldung erfolgreich.');
+        return true;
+    } else {
+        console.log('Falscher Benutzername oder Passwort.');
+        return false;
+    }
+}
+registerUser('Benutzer1', 'Passwort123');
+loginUser('Benutzer1', 'Passwort123');

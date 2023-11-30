93011082c5059d30eca37dae52fc87d6ebfd16e3
PatrickZablocki <Patrick Zablocki>
2023-11-30 09:46:51 +0100

    Versuche es das wenn wir eingeloggt sind, es auch bei jeder seite bleiben

--- JS/Login.js
+++ JS/Login.js
@@ -5,18 +5,35 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
     const password = document.getElementById('passwordInput').value;
 
     fetch('users.json')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Fehler beim Laden der Benutzerdaten.');
+            }
+            return response.json();
+        })
         .then(data => {
             console.log('Benutzerdaten aus JSON:', data);
 
             const user = data.users.find(user => user.email === email && user.password === password);
             if (user) {
                 console.log('Erfolgreich eingeloggt.');
-                
                 window.location.href = '/Eingeloggt.html';
             } else {
                 console.log('Ungültige Anmeldeinformationen.');
+                
             }
         })
-        .catch(error => console.error('Fehler beim Laden der Benutzerdaten:', error));
+        .catch(error => {
+            console.error('Fehler beim Laden oder Verarbeiten der Benutzerdaten:', error.message);
+            
+        });
 });
+
+
+
+
+
+
+
+
+
